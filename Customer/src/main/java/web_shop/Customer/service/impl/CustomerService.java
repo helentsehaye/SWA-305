a9ae8a1d12a334dcd3ab33552e10fe6eb745aceb
eden54514 <112902624+eden54514@users.noreply.github.com>
2023-02-21 02:22:24 -0600

    Update CustomerService.java
    update is left

--- Customer/src/main/java/web_shop/Customer/service/impl/CustomerService.java
+++ Customer/src/main/java/web_shop/Customer/service/impl/CustomerService.java
@@ -1,4 +1,50 @@
 package web_shop.Customer.service.impl;
 
-public class CustomerService {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.cfg.MapperBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web_shop.Customer.domain.Customer;
+import web_shop.Customer.dto.CustomerDto;
+import web_shop.Customer.repository.CustomerRepo;
+import web_shop.Customer.service.CustomerServiceInterface;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerService implements CustomerServiceInterface {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public List<CustomerDto> getCustomers(){
+        List<Customer> customers= customerRepo.findAll();
+
+        List<CustomerDto> customerDtos = customers.stream()
+                .map(x->objectMapper.convertValue(x,CustomerDto.class))
+                .collect(Collectors.toList());
+        return customerDtos;
+    }
+    public CustomerDto getCustomer(Long id){
+
+     return objectMapper.convertValue(customerRepo.findById(id).get(),CustomerDto.class);
+    }
+    public void createCustomer(CustomerDto customerDto){
+        customerRepo.save(objectMapper.convertValue(customerDto,Customer.class));
+    }
+//    public CustomerDto updateCustomer(Long id , CustomerDto customerDto){
+//
+//        CustomerDto customerDto1=objectMapper.convertValue(customerRepo.findById(id).get(),CustomerDto.class);
+//        customerDto1 =customerDto;
+//        customerRepo.save(customerDto1);
+//
+//        return customerDto1;
+//
+//    }
+    public void deleteCustomer(Long id){
+
+        customerRepo.delete(customerRepo.findById(id).get());
+    }
 }
