b7642ba53501562f030e09cc3dc1a030ab1a7042
eden54514 <112902624+eden54514@users.noreply.github.com>
2023-02-21 02:21:52 -0600

    Update CustomerController.java
    not complete yet

--- Customer/src/main/java/web_shop/Customer/controller/CustomerController.java
+++ Customer/src/main/java/web_shop/Customer/controller/CustomerController.java
@@ -1,4 +1,14 @@
 package web_shop.Customer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import web_shop.Customer.service.impl.CustomerService;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    
 }
