a576b221914d60c9d83fe1cde6455f3f4050d02d
eden54514 <112902624+eden54514@users.noreply.github.com>
2023-02-21 15:01:45 -0600

    just complete and left with @mapping

--- Customer/src/main/java/web_shop/Customer/controller/CustomerController.java
+++ Customer/src/main/java/web_shop/Customer/controller/CustomerController.java
@@ -3,7 +3,10 @@ package web_shop.Customer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import web_shop.Customer.dto.CustomerDto;
 import web_shop.Customer.service.impl.CustomerService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(name = "/customer")
@@ -11,6 +14,37 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+
+    public CustomerDto getCustomer(Long id){
+       return customerService.getCustomer(id);
+    }
+
+    public List<CustomerDto> getCustomers(){
+        return customerService.getCustomers();
+    }
+
+    public void createCustomer(CustomerDto customerDto){
+        customerService.createCustomer(customerDto);
+    }
+
+    public CustomerDto updateCustomer(Long id , CustomerDto customerDto){
+        return customerService.updateCustomer(id,customerDto);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
