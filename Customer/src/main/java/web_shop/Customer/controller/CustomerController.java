f1450a3882ae2f0ac21e4b1757321a3c5def0688
eden54514 <112902624+eden54514@users.noreply.github.com>
2023-02-21 14:03:56 -0600

    Update CustomerController.java
    add a @RequestMapping(name = "/customer")

--- Customer/src/main/java/web_shop/Customer/controller/CustomerController.java
+++ Customer/src/main/java/web_shop/Customer/controller/CustomerController.java
@@ -1,10 +1,12 @@
 package web_shop.Customer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import web_shop.Customer.service.impl.CustomerService;
 
 @RestController
+@RequestMapping(name = "/customer")
 public class CustomerController {
 
     @Autowired
