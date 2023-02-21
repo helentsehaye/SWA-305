d503d26b538e371167f3c827b72dc52bb6f768de
eden54514 <112902624+eden54514@users.noreply.github.com>
2023-02-21 01:32:26 -0600

    Update CustomerRepo.java

--- Customer/src/main/java/web_shop/Customer/repository/CustomerRepo.java
+++ Customer/src/main/java/web_shop/Customer/repository/CustomerRepo.java
@@ -1,4 +1,8 @@
 package web_shop.Customer.repository;
 
-public class CustomerRepo {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import web_shop.Customer.domain.Customer;
+@Repository
+public interface CustomerRepo extends MongoRepository<Customer,Long> {
 }
