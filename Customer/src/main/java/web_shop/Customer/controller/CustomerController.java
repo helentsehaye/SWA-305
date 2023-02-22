187ea197ac29cd415a3efcbe2dc66987ef685d61
eden54514 <112902624+eden54514@users.noreply.github.com>
2023-02-21 19:56:44 -0600

    just complete

--- Customer/src/main/java/web_shop/Customer/controller/CustomerController.java
+++ Customer/src/main/java/web_shop/Customer/controller/CustomerController.java
@@ -1,33 +1,33 @@
 package web_shop.Customer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 import web_shop.Customer.dto.CustomerDto;
 import web_shop.Customer.service.impl.CustomerService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "/customer")
+@RequestMapping( "/customer")
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
-
-    public CustomerDto getCustomer(Long id){
+    @GetMapping("/get/{id}")
+    public CustomerDto getCustomer(@PathVariable Long id){
        return customerService.getCustomer(id);
     }
-
+    @GetMapping( "/list")
     public List<CustomerDto> getCustomers(){
         return customerService.getCustomers();
     }
-
+    @PostMapping( "/create")
     public void createCustomer(CustomerDto customerDto){
         customerService.createCustomer(customerDto);
     }
-
-    public CustomerDto updateCustomer(Long id , CustomerDto customerDto){
+    @PutMapping("/update/{id}")
+    public CustomerDto updateCustomer(@PathVariable Long id , CustomerDto customerDto){
         return customerService.updateCustomer(id,customerDto);
     }
 
